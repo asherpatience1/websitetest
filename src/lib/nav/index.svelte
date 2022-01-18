6e9bf5b20e2960acfbab487d1da7eccb94547f40
TheDoctor <m@emdienn.com>
2022-01-18 21:40:41 +1100

    Improve nav accessibility

--- src/lib/nav/index.svelte
+++ src/lib/nav/index.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+  import { page } from '$app/stores'
+
   const links = [
-    // { label: 'About Us', href: 'about' },
-    { label: 'Services', href: 'services' },
-    { label: 'Projects', href: 'projects' },
-    { label: 'Contact', href: 'contact' },
+    { label: 'Services', href: '/services' },
+    { label: 'Projects', href: '/projects' },
+    { label: 'Contact', href: '/contact' },
   ]
 </script>
 
 <nav>
   <ul>
     {#each links as { href, label }}
-      <li><a {href}>{label}</a></li>
+      <li class:selected={$page.url.pathname === href}><a {href}>{label}</a></li>
     {/each}
   </ul>
 </nav>
@@ -23,7 +24,12 @@
     gap: 2rem;
     padding: 0;
   }
+  nav .selected a {
+    background: #5599ff;
+  }
   nav li a {
+    text-transform: uppercase;
+    transition: all 720ms cubic-bezier(0.2, 1, 0, 1);
     background: #0177ff;
     color: white;
     padding: 0.5rem;
@@ -35,6 +41,9 @@
     text-align: center;
     font-size: 1.125rem;
   }
+  nav li a:hover, nav li a:focus {
+    background-color: #014480;
+  }
 
   @media screen and (max-width: 560px) {
     nav ul {
@@ -42,6 +51,14 @@
     }
   }
   @media screen and (max-width: 500px) {
+    nav ul {
+      gap: 1rem;
+    }
+    nav li a {
+      width: 6rem;
+    }
+  }
+  @media screen and (max-width: 400px) {
     nav ul {
       gap: 0.5rem;
     }
