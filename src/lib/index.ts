9887378a42f5a065c43ce50b8b3cc764d372d79e
TheDoctor <m@emdienn.com>
2022-01-20 14:31:44 +1100

    Expose carousel from library root

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './header/index.svelte'
 export { default as Nav } from './nav/index.svelte'
 export { default as World } from './world/index.svelte'
+export { default as Carousel } from './carousel/index.svelte'
