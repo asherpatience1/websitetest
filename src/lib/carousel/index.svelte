0a4d23bc872f163adcba631122c4061d4db30024
TheDoctor <m@emdienn.com>
2022-01-20 16:55:08 +1100

    Remove dummy placeholders

--- src/lib/carousel/index.svelte
+++ src/lib/carousel/index.svelte
@@ -14,21 +14,5 @@
 </script>
 
 <svelte:component this={Carousel} {autoplay} {particlesToShow} {duration} {arrows} {timingFunction}>
-  <h4>CLICK Engineering</h4>
-  <h4>BHP</h4>
-  <h4>LandSurveys</h4>
-  <h4>Veris</h4>
-  <h4>Hardi</h4>
   <slot />
 </svelte:component>
-
-<style>
-  h4 {
-    text-align: center;
-    background: #fff7;
-    padding: 2rem;
-    /* margin: 2px; */
-    border: solid 2px #0003;
-    box-sizing: border-box;
-  }
-</style>
