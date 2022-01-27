be9f61d178cc3e72c5aaa3f2f731247028618b7a
TheDoctor <m@emdienn.com>
2022-01-28 10:34:35 +1100

    Add reCaptcha ENV options

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly SENDINBLUE_APIKEY: string
   readonly SENDER_ADDR: string
   readonly SENDER_NAME: string
+  readonly RECAPTCHA_SECRET_KEY: string
+  readonly VITE_RECAPTCHA_SITE_KEY: string
 }
 
 interface ImportMeta {
