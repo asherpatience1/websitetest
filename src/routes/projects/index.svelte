3c9166e674218cd0c29c5dd67c73aea722077dbb
TheDoctor <m@emdienn.com>
2022-02-09 15:05:31 +1100

    Auto-detect projects on project index

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,3 +1,35 @@
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit'
+
+  const prefix = './_content/'
+  const suffix = '/index.svelte'
+
+  const projects = import.meta.glob('./_content/**/index.svelte')
+  const metas = import.meta.glob('./_content/**/meta.json')
+
+  export const load: Load = async () => {
+    const list = []
+
+    for (const key in projects) {
+      const dir = key.slice(prefix.length, -suffix.length)
+      const href = dir.slice(3)
+      const meta = (await metas[`./_content/${dir}/meta.json`]()).default
+      list.push({ href, meta })
+    }
+
+    return {
+      props: {
+        projects: list
+      }
+    }
+  }
+
+</script>
+
+<script lang="ts">
+  export let projects: any[]
+</script>
+
 <svelte:head>
   <title>DIOVEC - Projects</title>
 </svelte:head>
@@ -5,4 +37,6 @@
 <h2>Projects</h2>
 <p>We can has projects done gud.</p>
 
-<h3><a href="/projects/example">Example</a></h3>
+{#each projects as { href, meta }}
+  <h3><a href="/projects/{href}">{meta.title}</a></h3>
+{/each}
